b7907502d2b98c321954b38ecaceefec73f4a8cb
adamska <dooody94@gmail.com>
2020-05-28 17:41:33 +0300

    Added RU DE UK localizations

--- app/src/main/java/com/example/android/bactrack/AboutActivity.java
+++ app/src/main/java/com/example/android/bactrack/AboutActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.bactrack;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,8 +16,12 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        TextView textAbout = findViewById(R.id.textAbout);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            textAbout.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
+        }
 
-        Button backButton = (Button) findViewById(R.id.backButton);
+        Button backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the phrases category is clicked on.
             @Override
